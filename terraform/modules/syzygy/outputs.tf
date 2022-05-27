7043983a782cf56de44624a88284b4b69a3a08b3
Ian Allison <iana@pims.math.ca>
2022-05-26 17:54:18 -0700

    Bulk import of updated roles
    Signed-off-by: Ian Allison <iana@pims.math.ca>

--- terraform/modules/syzygy/outputs.tf
+++
@@ -1,8 +0,0 @@
-#output "hub_ip" {
-#  value = libvirt_domain.hub.network_interface[0].addresses[0]
-#}
-
-#output "mark_ip" {
-#  value = libvirt_domain.marking.network_interface[0].addresses[0]
-#}
-
--- terraform/prod/outputs.tf
+++
@@ -1,8 +0,0 @@
-#output "hub_ip" {
-#  value = module.syzygy.hub_ip
-#}
-
-#output "mark_ip" {
-#  value = module.syzygy.mark_ip
-#}
-
